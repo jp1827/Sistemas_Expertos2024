fb76f5dc1a8aafee5be93b4e2850a7c74c25e287
jp1827 <90116911+jp1827@users.noreply.github.com>
2024-08-03 12:38:52 -0600

    Encadenamiento - Con ingreso de hechos
    Este permite el ingreso de hechos y la validación en caso de formular conclusiones

--- Encadenamiento.py
+++ Encadenamiento.py
@@ -1,3 +1,5 @@
+import os
+
 class Regla:
     def __init__(self, condiciones, conclusion):
         self.condiciones = condiciones
@@ -18,22 +20,46 @@ def encadenamiento_hacia_adelante(hechos, reglas):
     
     return conclusiones
 
+def Ingresar_hechos():
+    hechos = set()
+    print("Ingrese los hechos sobre los cientes de bancos:")
+    hecho = input("").strip().lower()
+    hechos.add(hecho)
+    
+    print("Desea ingresar otro hecho:")
+    print("1 para sí")
+    print("2 para no")
+    opcion = input("")
+    if opcion == "1":
+        Ingresar_hechos()
+    elif opcion == "2":
+        return  hechos
+    else:
+        print("Ingreso de hechos ha sido finalizada por opción erronea")
+        return hechos
+
+
 def main():
-    hechos = {"Cuenta bancaria", "Pago puntual", "Leal", "Moroso", "Cliente frequente", "Cliente regular", "Cliente ocasional"}
+    hechos = Ingresar_hechos()
 
     reglas = [
-        Regla({"Cuenta bancaria"}, "Cliente"),
-        Regla({"Cuenta bancaria", "Pago puntual", "Leal", "Cliente frequente"}, "Cliente A"),
-        Regla({"Cuenta bancaria", "Pago puntual", "Cliente regular"}, "Cliente B"),
-        Regla({"Cuenta bancaria", "Pago puntual", "Cliente ocasional"}, "Cliente C"),
-        Regla({"Cuenta bancaria", "Moroso", "Cliente ocasional"}, "Cliente D"),
+        Regla({"cuenta bancaria"}, "Cliente del banco"),
+        Regla({"cuenta bancaria", "pago puntual", "leal", "cliente frequente"}, "cliente A"),
+        Regla({"cuenta bancaria", "pago puntual", "cliente regular"}, "cliente B"),
+        Regla({"cuenta bancaria", "pago puntual", "cliente ocasional"}, "cliente C"),
+        Regla({"cuenta bancaria", "moroso", "cliente ocasional"}, "cliente D"),
     ]
 
     conclusiones = encadenamiento_hacia_adelante(hechos, reglas)
 
-    print("Posibles tipos de clientes:")
-    for conclusion in conclusiones:
-        print(conclusion)
+    if not conclusiones:
+            print("No se encontraron conclusiones con los hechos ingresados")
+    else:
+        print("Posibles tipos de clientes:")
+        for conclusion in conclusiones:
+            print(conclusion)
+    
+        
 
 if __name__ == "__main__":
     main()
